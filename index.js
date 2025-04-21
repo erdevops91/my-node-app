0a933d5fe6ea3a7ba6abc11f38492413a7954e71
Rohit Kumar Singh <erdevops91@gmail.com>
2025-04-21 17:32:34 +0530

    my new change for date

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello Rohit Singh ji....new request from Node.js app on GCP!');
+  res.send('Hello Rohit Singh ji.... date 21-apr-25 new request from Node.js app on GCP!');
 });
 
 app.listen(port, () => {
