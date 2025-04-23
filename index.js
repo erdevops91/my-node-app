6a63ee77a0dfcfd22b60c7da630f8d61e731d18d
Rohit Kumar Singh <erdevops91@gmail.com>
2025-04-23 12:31:28 +0530

    change in indix page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello Rahul Singh ====>>> WELCOME TO MY HOME PAGE=====>>> new request from Node.js app on GCP!');
+  res.send('Hello Anil Singh ====>>> WELCOME TO MY HOME PAGE=====>>> new request from Node.js app on GCP!');
 });
 
 app.listen(port, () => {
