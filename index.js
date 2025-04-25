d79d42f2c732b14fe90a79b32524f237421b6082
Rohit Kumar Singh <erdevops91@gmail.com>
2025-04-25 17:46:03 +0530

    ingree file updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello Ahish sir  ====>>> WELCOME TO MY HOME PAGE=====>>> new request from Node.js app on GCP!');
+  res.send('Hello Rohit this is a testing for ingress setup ====>>> WELCOME TO MY HOME PAGE=====>>> new request from Node.js app on GCP!');
 });
 
 app.listen(port, () => {
